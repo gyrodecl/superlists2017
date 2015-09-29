3b147fb01be1e12ff3571b2fb5fa25f41f5d64ef
gyro <gyrcl@outlook.com>
2015-09-29 00:35:43 -0700

    first unit test passing. now have url mapping

--- lists/urls.py
+++ lists/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
-
+from lists import views
 
 urlpatterns = [
-    url(r'^$', ,)
+    url(r'^$', views.home_page, name='home'),
 ]
--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.contrib import messages
+from django.core.urlresolvers import reverse
+from django.utils import timezone
+from django.views import generic
 
-# Create your views here.
+def home_page(request):
+    return HttpResponse("hi")
+
