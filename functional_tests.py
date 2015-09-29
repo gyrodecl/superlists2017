6d523e8f6a76a0a7e9756fac5ddc35adb3be680b
gyro <gyrcl@outlook.com>
2015-09-29 14:55:08 -0700

    functional tests now input item onto page

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,13 +17,35 @@ class NewVisitorTest(unittest.TestCase):
 
         #sees the title
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         #invited to enter a to-do item straight away
         #she types "Buy peacock feathers" into text box
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         #when she hits enter, the page updates, and now the page lists
         #"1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys("Buy peacock feathers")
+        
+        #hits enter
+        inputbox.send_keys(Keys.ENTER)
+        
+        #now look at the table of results
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+    
+
+        self.fail('Finish the test!')
+
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
