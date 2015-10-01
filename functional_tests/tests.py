3d3693926b767ed4a98229e08b10d5f40f212365
gyro <gyrcl@outlook.com>
2015-09-30 19:21:02 -0700

    functional tests now using regex

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-
 class NewVisitorTest(LiveServerTestCase):
     
     def setUp(self):
@@ -43,10 +42,13 @@ class NewVisitorTest(LiveServerTestCase):
         #"1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys("Buy peacock feathers")
         inputbox.send_keys(Keys.ENTER)
+        #since this is the home page, she has created a new list
+        #with its own URL;
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         
         #import time
-        #time.sleep(10)
-        
+        #time.sleep(10)       
         #now look at the table of results
         self.check_for_row_in_list_table("1: Buy peacock feathers")
     
@@ -59,8 +61,37 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table("1: Buy peacock feathers")
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         
+        #Now a new user, Francis, comes along to the site.
+        ##We use a new browser session to make sure no info
+        #of Edith's is coming through from cookies
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        
+        #Francis visits the home page. no sign of edit's list
+        self.browser.get(self.live.server_url + '/lists/')
+        page_text = self.browser.find_element_by_tag_name('body')
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+        
+        #Francis starts a new list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        
+        #Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+        
+        #Again, no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body')
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+        
+        #satisfied they go back to sleep
+        
         self.fail('Finish the test!')
-
+    
 
 '''
 #don't need this anymore since using django test runner
