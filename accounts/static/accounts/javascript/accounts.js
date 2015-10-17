5bfce4c4965afb2cc9809a39237e6c1b1a2ed2a7
gyro <gyrcl@outlook.com>
2015-10-16 20:13:52 -0700

    complete javascript client-side work for authentication with mozilla browser-id

--- accounts/static/accounts/javascript/accounts.js
+++ accounts/static/accounts/javascript/accounts.js
@@ -1,7 +1,19 @@
-var initialize = function(navigator) {
+var initialize = function(navigator, user, token, urls) {
     console.log(navigator);
     $('#id_login').click(function(event) {
             navigator.id.request(); 
+    });
+    
+    navigator.id.watch({
+        loggedInUser: user,
+        onlogin: function(assertion) {
+            $.post(urls.login, {'assertion': assertion, 'csrfmiddlewaretoken': token})
+             .done(function(){window.location.reload();})
+             .fail(function() {navigator.id.logout();});
+        },
+        onlogout: function() {
+            $.post(urls.logout)
+        }
     });
 };
     
