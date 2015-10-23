0843c05dae33661f6e9e220dd2b7032e3e2f8a07
gyro <gyrcl@outlook.com>
2015-10-22 22:35:43 -0700

    now can do sessions on the server

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -6,24 +6,33 @@ from selenium.webdriver.support.ui import WebDriverWait
 import unittest
 import sys
 
+#ch17
+from .server_tools import reset_database
+
 #ch.10 Refactor Functional Tests into Many Files
 class FunctionalTest(StaticLiveServerTestCase):
     
     @classmethod
     def setUpClass(cls):
         for arg in sys.argv:  
-            if 'liveserver' in arg:  
-                cls.server_url = 'http://' + arg.split('=')[1]  
+            if 'liveserver' in arg:
+                cls.server_host = arg.split('=')[1]
+                cls.server_url = 'http://' + cls.server_host
+                cls.against_staging = True
                 return  
-        super().setUpClass()  
+        super().setUpClass()
+        cls.against_staging = False
         cls.server_url = cls.live_server_url
     
     @classmethod
     def tearDownClass(cls):
-        if cls.server_url == cls.live_server_url:
+        if not cls.against_staging:
+        #if cls.server_url == cls.live_server_url:
             super().tearDownClass()
     
     def setUp(self):
+        if self.against_staging:
+            reset_database(self.server_host)
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
     
--- superlists2015/settings.py
+++ superlists2015/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
-    'accounts'
+    'accounts',
+    'functional_tests',
 )
 
 MIDDLEWARE_CLASSES = (
